b92bff5463054141b9e7dd66ea91e32517d93d0e
NesyElimelech <nesy15@gmail.com>
2024-10-13 08:58:48 +0300

    Add auth so we can login with Google and Github, and shadcn-ui, connect to neon db with drizzle

---
+++ server/auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/server";
+import { users, accounts } from "@/server/schema";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: DrizzleAdapter(db, { usersTable: users, accountsTable: accounts }),
+  secret: process.env.AUTH_SECRET,
+  session: { strategy: "jwt" },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
